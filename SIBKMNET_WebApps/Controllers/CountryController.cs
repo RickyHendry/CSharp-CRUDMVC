353f4b13793e9805520ffce4d977036f63debdbf
Hendri <rickyhendry05@gmail.com>
2022-09-26 13:18:04 +0800

    Add project files.

---
+++ SIBKMNET_WebApps/Controllers/CountryController.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SIBKMNET_WebApps.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKMNET_WebApps.Controllers
+{
+    public class CountryController : Controller
+    {
+        SqlConnection sqlConnection;
+
+        /*
+         * Data Source -> Server
+         * Initial Catalog -> Database
+         * User ID -> username
+         * Password -> password
+         * Connect Timeout
+         */
+        string connectionString = "Data Source=HENDRI;Initial Catalog=SIBKMNET;" +
+            "User ID=sibkmnet;Password=123456;Connect Timeout=30;";
+
+        public object Id { get; private set; }
+        public object country { get; private set; }
+
+        // GET ALL
+        // GET
+        public IActionResult Index()
+        {
+            string query = "SELECT * FROM Country";
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            List<Country> Countries = new List<Country>();
+
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Country country = new Country();
+                            country.Id = Convert.ToInt32(sqlDataReader[0]);
+                            country.Name = sqlDataReader[1].ToString();
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1]);
+                            Countries.Add(country);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+            return View(Countries);
+        }
+
+        // GET BY ID
+        // GET
+        public IActionResult GetById()
+        {
+            string query = "SELECT * FROM Country WHERE Id = @id";
+
+            SqlParameter sqlParameter = new SqlParameter();
+            sqlParameter.ParameterName = "@id";
+            sqlParameter.Value = Id;
+            List<Country> Countries = new List<Country>();
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.Add(sqlParameter);
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Country country = new Country();
+                            country.Id = Convert.ToInt32(sqlDataReader[0]);
+                            country.Name = sqlDataReader[1].ToString();
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1]);
+                            Countries.Add(country);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+
+            return View(Countries);
+        }
+
+        // CREATE
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Country country)
+        {
+            return View();
+        }
+
+        // UPDATE
+        // GET
+        public IActionResult Update()
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+                List<Country> Countries = new List<Country>();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@name";
+                //sqlParameter.Value = country.Name;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+
+
+                try
+                {
+                    sqlCommand.CommandText = "UPDATE Country SET name = (@update)" + "WHERE Id = (@id)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+            return View();
+        }
+        // POST
+
+
+        // DELETE
+        // GET
+        // POST
+    }
+}
